1e338b16334e7fd089da702445ee6656a9486b94
Mecamodore <mecamodore@126.com>
2024-07-10 19:22:36 +0800

    新加了第一个练习，有关转义字符

---
+++ JavaProjects/Practice/src/ChangeChar_ex01.java
@@ -0,0 +1,14 @@
+//这个程序主要是用来练习转义字符
+//上面一排显示表头，下面一排显示数据
+public class ChangeChar_ex01 {
+    public static void main(String[] args){
+        System.out.print("书名\t");
+        System.out.print("作者\t");
+        System.out.print("价格\t");
+        System.out.print("销量\t\n");
+        System.out.print("三国\t");
+        System.out.print("罗贯中\t");
+        System.out.print("120\t");
+        System.out.print("1000\t");
+    }
+}
