d48c22cb9e82b8cb16c08ec4633131928dab6b6b
Mecamodore <mecamodore@126.com>
2024-07-11 21:19:31 +0800

    第三章课后习题

---
+++ JavaProjects/Practice/src/Chapter03/char_ex02.java
@@ -0,0 +1,20 @@
+package Chapter03;
+
+public class char_ex02 {
+    public static void main(String[] args) {
+        char ch_n = '\n';
+        char ch_t = '\t';
+        char ch_r = '\r';
+        char ch = '\\';
+        char ch_1 = '1';
+        char ch_2 = '2';
+        char ch_3 = '3';
+        System.out.println(ch_n+"200");
+        System.out.println(ch_t+"200");
+        System.out.println("1111"+ch_r+"20");
+        System.out.println(ch);
+        System.out.println(ch_1);
+        System.out.println(ch_2);
+        System.out.println(ch_3);
+    }
+}
